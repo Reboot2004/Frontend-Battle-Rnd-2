5784fddfa658b2358a295ace71358f1e86f5e9ef
Reboot2004 <142259578+Reboot2004@users.noreply.github.com>
2024-05-19 11:10:53 +0530

    Delete Frontend-Battle Round 2 directory

--- Frontend-Battle Round 2/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggler = document.getElementById('navbar-toggler');
-    const navbarNav = document.getElementById('navbarNav');
-
-    toggler.addEventListener('click', function () {
-        navbarNav.classList.toggle('show');
-    });
-});
